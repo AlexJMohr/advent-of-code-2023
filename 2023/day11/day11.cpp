6c88a5448a6b3fd4e52217a571c65948961af477
Alex Mohr <mohralexj@gmail.com>
2023-12-15 12:16:29 -0800

    day 11 done

--- 2023/day11/day11.cpp
+++ 2023/day11/day11.cpp
@@ -6,7 +6,6 @@
 #include <numeric>
 
 typedef std::pair<int, int> coord;
-
 typedef std::vector<std::string> grid;
 
 grid parse_file(const char *filename)
@@ -21,6 +20,16 @@ grid parse_file(const char *filename)
     return grid;
 }
 
+std::vector<coord> find_galaxy_positions(grid const &grid)
+{
+    std::vector<coord> galaxy_positions;
+    for (size_t y = 0; y < grid.size(); ++y)
+        for (size_t x = 0; x < grid[y].size(); ++x)
+            if (grid[y][x] == '#')
+                galaxy_positions.push_back({x, y});
+    return galaxy_positions;
+}
+
 std::vector<std::pair<coord, coord>> get_galaxy_pairs(std::vector<coord> galaxy_positions)
 {
     // permutations of all galaxy pairs
@@ -31,171 +40,89 @@ std::vector<std::pair<coord, coord>> get_galaxy_pairs(std::vector<coord> galaxy_
     return pairs;
 }
 
-int manhattan_distance(coord c1, coord c2)
+std::vector<std::vector<int>> get_expansion_grid(const grid &grid, int expansion)
 {
-    return std::abs(c1.first - c2.first) + std::abs(c1.second - c2.second);
+    std::vector<std::vector<int>> expansion_grid(grid.size(), std::vector<int>(grid[0].size(), 1));
+    // find empty rows
+    for (size_t y = 0; y < grid.size(); ++y)
+    {
+        bool empty_row = true;
+        for (size_t x = 0; x < grid[y].size(); ++x)
+        {
+            if (grid[y][x] == '#')
+            {
+                empty_row = false;
+                break;
+            }
+        }
+        if (empty_row)
+            expansion_grid[y] = std::vector<int>(grid[0].size(), expansion);
+    }
+    // find empty columns
+    for (size_t x = 0; x < grid[0].size(); ++x)
+    {
+        bool empty_col = true;
+        for (size_t y = 0; y < grid.size(); ++y)
+        {
+            if (grid[y][x] == '#')
+            {
+                empty_col = false;
+                break;
+            }
+        }
+        if (empty_col)
+            for (size_t y = 0; y < grid.size(); ++y)
+                expansion_grid[y][x] = expansion;
+    }
+    return expansion_grid;
+}
+
+int64_t get_total_distance(std::vector<coord> const &galaxy_positions, std::vector<std::vector<int>> const &expansion_grid)
+{
+    auto galaxy_pairs = get_galaxy_pairs(galaxy_positions);
+
+    int64_t total_distance = 0;
+    for (const auto pair : galaxy_pairs)
+    {
+        auto g1 = pair.first;
+        auto g2 = pair.second;
+
+        int64_t horizontal_distance = 0;
+        // start on east-most galaxy
+        if (g1.first > g2.first)
+            std::swap(g1, g2);
+        for (int x = g1.first; x < g2.first; ++x)
+            horizontal_distance += expansion_grid[g1.second][x];
+        int64_t vertical_distance = 0;
+        // start on north-most galaxy
+        if (g1.second > g2.second)
+            std::swap(g1, g2);
+        for (int y = g1.second; y < g2.second; ++y)
+            vertical_distance += expansion_grid[y][g2.first];
+        total_distance += horizontal_distance + vertical_distance;
+    }
+    return total_distance;
 }
 
 void part1(const char *filename)
 {
     auto grid = parse_file(filename);
-
-    std::vector<int> empty_row_indices;
-    std::vector<int> empty_col_indices;
-    // find empty rows and galaxy positions
-    for (size_t y = 0; y < grid.size(); ++y)
-    {
-        bool empty_row = true;
-        for (size_t x = 0; x < grid[y].size(); ++x)
-        {
-            if (grid[y][x] == '#')
-            {
-                empty_row = false;
-                break;
-            }
-        }
-        if (empty_row)
-            empty_row_indices.push_back(y);
-    }
-    // find empty columns
-    for (size_t x = 0; x < grid[0].size(); ++x)
-    {
-        bool empty_col = true;
-        for (size_t y = 0; y < grid.size(); ++y)
-        {
-            if (grid[y][x] == '#')
-            {
-                empty_col = false;
-                break;
-            }
-        }
-        if (empty_col)
-            empty_col_indices.push_back(x);
-    }
-
-    // clone empty rows and columns.
-    // reverse so that the insertions don't mess up the indices.
-    std::reverse(empty_row_indices.begin(), empty_row_indices.end());
-    std::reverse(empty_col_indices.begin(), empty_col_indices.end());
-    for (auto const &col : empty_col_indices)
-        for (auto &row : grid)
-            row.insert(row.begin() + col, '.');
-    for (auto const &row : empty_row_indices)
-        grid.insert(grid.begin() + row, std::string(grid[0].size(), '.'));
-
-    // find galaxy positions ('#')
-    std::vector<coord> galaxy_positions;
-    for (size_t y = 0; y < grid.size(); ++y)
-        for (size_t x = 0; x < grid[y].size(); ++x)
-            if (grid[y][x] == '#')
-                galaxy_positions.push_back({x, y});
-
-    auto pairs = get_galaxy_pairs(galaxy_positions);
-
-    int total_distance = 0;
-    for (const auto &[g1, g2] : pairs)
-        total_distance += manhattan_distance(g1, g2);
-
+    auto galaxy_positions = find_galaxy_positions(grid);
+    auto expansion_grid = get_expansion_grid(grid, 2);
+    auto total_distance = get_total_distance(galaxy_positions, expansion_grid);
     std::cout << "part 1: " << total_distance << std::endl; // 9550717
 }
 
 void part2(const char *filename)
 {
+    // const int EXPANSION = 10; // for test, and total distance should be 1030
+    // const int EXPANSION = 100; // for test, and total distance should be 8410
+    const int EXPANSION = 1000000; // for input
     auto grid = parse_file(filename);
-
-    std::vector<int> empty_row_indices;
-    std::vector<int> empty_col_indices;
-    // find empty rows and galaxy positions
-    for (size_t y = 0; y < grid.size(); ++y)
-    {
-        bool empty_row = true;
-        for (size_t x = 0; x < grid[y].size(); ++x)
-        {
-            if (grid[y][x] == '#')
-            {
-                empty_row = false;
-                break;
-            }
-        }
-        if (empty_row)
-            empty_row_indices.push_back(y);
-    }
-    // find empty columns
-    for (size_t x = 0; x < grid[0].size(); ++x)
-    {
-        bool empty_col = true;
-        for (size_t y = 0; y < grid.size(); ++y)
-        {
-            if (grid[y][x] == '#')
-            {
-                empty_col = false;
-                break;
-            }
-        }
-        if (empty_col)
-            empty_col_indices.push_back(x);
-    }
-
-    // print empty rows and columns
-    std::cout << "empty rows: ";
-    for (auto const &row : empty_row_indices)
-        std::cout << row << ' ';
-    std::cout << std::endl;
-    std::cout << "empty cols: ";
-    for (auto const &col : empty_col_indices)
-        std::cout << col << ' ';
-    std::cout << std::endl;
-
-    // find galaxy positions ('#') before the expansion
-    std::vector<coord> galaxy_positions;
-    for (size_t y = 0; y < grid.size(); ++y)
-        for (size_t x = 0; x < grid[y].size(); ++x)
-            if (grid[y][x] == '#')
-                galaxy_positions.push_back({x, y});
-
-    auto pairs = get_galaxy_pairs(galaxy_positions);
-
-    const int expansion = 10; // for test, and total distance should be 1030
-    // const int expansion = 1'000'000; // for input
-
-    int total_distance = 0;
-    for (const auto &[g1, g2] : pairs)
-    {
-        int x1 = g1.first;
-        int y1 = g1.second;
-        int x2 = g2.first;
-        int y2 = g2.second;
-
-        // make sure x1 < x2 and y1 < y2
-        if (x1 > x2)
-            std::swap(x1, x2);
-        if (y1 > y2)
-            std::swap(y1, y2);
-
-        // find number of empty columns between x and g2.first
-        auto first_empty_col = std::lower_bound(empty_col_indices.begin(), empty_col_indices.end(), x1);
-        auto last_empty_col = std::lower_bound(empty_col_indices.begin(), empty_col_indices.end(), x2);
-        int num_empty_cols = std::distance(first_empty_col, last_empty_col);
-
-        auto first_empty_row = std::lower_bound(empty_row_indices.begin(), empty_row_indices.end(), y1);
-        auto last_empty_row = std::lower_bound(empty_row_indices.begin(), empty_row_indices.end(), y2);
-        int num_empty_rows = std::distance(first_empty_row, last_empty_row);
-
-        std::cout << "num_empty_cols: " << num_empty_cols << std::endl;
-        std::cout << "num_empty_rows: " << num_empty_rows << std::endl;
-
-        int horizontal_distance = x2 - x1 - num_empty_cols + (num_empty_cols * expansion);
-        int vertical_distance = y2 - y1 - num_empty_rows + (num_empty_rows * expansion);
-        std::cout << "horizontal_distance: " << horizontal_distance << std::endl;
-        std::cout << "vertical_distance: " << vertical_distance << std::endl;
-        total_distance += horizontal_distance + vertical_distance;
-        std::cout << "---\n";
-        // break;
-    }
-
-    // 58000090 too low
-    // 82000210 too low
-    std::cout << "part 2: " << total_distance << std::endl;
+    auto galaxy_positions = find_galaxy_positions(grid);
+    auto expansion_grid = get_expansion_grid(grid, EXPANSION);
+    auto total_distance = get_total_distance(galaxy_positions, expansion_grid);
+    std::cout << "part 2: " << total_distance << std::endl; // 648458253817
 }
 
 int main(int argc, char *argv[])
