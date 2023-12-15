50c5b1557c917365a4c41cf4ca317f44f6ec474c
Alex Mohr <mohralexj@gmail.com>
2023-12-14 20:27:24 -0800

    day 10 part 2

--- 2023/day10/day10.cpp
+++ 2023/day10/day10.cpp
@@ -11,7 +11,19 @@
 typedef std::pair<int, int> coord;
 typedef std::vector<std::string> grid;
 
-coord find_start(const grid &grid)
+grid parse_file(const char *filename)
+{
+    std::ifstream file(filename);
+    if (!file.is_open())
+        throw std::runtime_error("Could not open file");
+    grid grid;
+    std::string line;
+    while (std::getline(file, line))
+        grid.push_back(line);
+    return grid;
+}
+
+coord find_start(grid const &grid)
 {
     for (int j = 0; j < grid.size(); ++j)
         for (int i = 0; i < grid[j].size(); ++i)
@@ -20,7 +32,7 @@ coord find_start(const grid &grid)
     throw std::runtime_error("Could not find start");
 }
 
-coord next_pos(const grid &grid, coord prev, coord current)
+coord next_pos(grid const &grid, coord prev, coord current)
 {
     auto &[px, py] = prev;
     auto &[cx, cy] = current;
@@ -43,6 +55,95 @@ coord next_pos(const grid &grid, coord prev, coord current)
     }
 }
 
+void part1(const char *filename)
+{
+    auto grid = parse_file(filename);
+    coord start = find_start(grid);
+    coord prev1 = start;
+    coord prev2 = start;
+    // next positions from start are east and west in both test and input
+    coord pos1 = {prev1.first + 1, prev1.second};
+    coord pos2 = {prev2.first, prev2.second + 1};
+    int step_count = 1;
+
+    while (pos1 != pos2)
+    {
+        auto temp1 = pos1;
+        auto temp2 = pos2;
+        pos1 = next_pos(grid, prev1, pos1);
+        pos2 = next_pos(grid, prev2, pos2);
+        prev1 = temp1;
+        prev2 = temp2;
+        ++step_count;
+    }
+
+    std::cout << "Part 1: " << step_count << std::endl; // 6968
+}
+
+std::vector<std::vector<bool>> find_loop_positions(grid const &grid, coord start)
+{
+    std::vector<std::vector<bool>> loop_positions(grid.size(), std::vector<bool>(grid[0].size(), false));
+    coord prev = start;
+    // next positions from start are south and east in both test and input.
+    // arbitrary choice of which to start with.
+    coord current = {start.first + 1, start.second};
+    loop_positions[start.second][start.first] = true;
+    while (true)
+    {
+        loop_positions[current.second][current.first] = true;
+        auto next = next_pos(grid, prev, current);
+        if (next == start)
+            break;
+        prev = current;
+        current = next;
+    }
+    return loop_positions;
+}
+
+void part2(const char *input)
+{
+    auto grid = parse_file(input);
+
+    coord start = find_start(grid);
+    // positions off of start are south and east in both test and input.
+    // so start must be 'F'
+    grid[start.second][start.first] = 'F';
+
+    // get grid of booleans where true means that the position is part of the loop
+    auto loop_positions_grid = find_loop_positions(grid, start);
+
+    // ray casting algorithm.
+    // for each point, cast a ray in any direction and count the number of intersections.
+    // if the number of intersections is odd, then the point is outside of the polygon.
+
+    int points_within = 0;
+    // loop over all points in the grid
+    for (int j = 0; j < grid.size(); ++j)
+    {
+        for (int i = 0; i < grid[j].size(); ++i)
+        {
+            // loop positions are not contained by the loop
+            if (loop_positions_grid[j][i])
+                continue;
+            // fire ray to the east and count the number of intersections.
+            // it is an intersection if the pipe has a south bend (i.e '|', 'F', '7')
+            int num_intersections = 0;
+            for (int x = i; x < grid[j].size(); ++x)
+            {
+                if (!loop_positions_grid[j][x])
+                    continue;
+                char pipe = grid[j][x];
+                if (pipe == '|' || pipe == 'F' || pipe == '7')
+                    ++num_intersections;
+            }
+            if (num_intersections % 2 == 1)
+                ++points_within;
+        }
+    }
+
+    std::cout << "Part 2: " << points_within << std::endl; // 413
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -55,41 +156,7 @@ int main(int argc, char *argv[])
     if (!file.is_open())
         throw std::runtime_error("Could not open file");
 
-    std::string line;
-
-    grid grid;
-    while (std::getline(file, line))
-        grid.push_back(line);
-
-    coord start = find_start(grid);
-    coord prev1 = start;
-    coord prev2 = start;
-    // next positions from start are east and west in both test and input
-    coord pos1 = {prev1.first + 1, prev1.second};
-    coord pos2 = {prev2.first, prev2.second + 1};
-    int step_count = 1;
-
-    // std::cout << "Step: " << step_count << '\n';
-    // std::cout << "1: " << prev1.first << ", " << prev1.second << " -> " << pos1.first << ", " << pos1.second << '\n';
-    // std::cout << "2: " << prev2.first << ", " << prev2.second << " -> " << pos2.first << ", " << pos2.second << '\n';
-    // std::cout << "---\n";
-
-    while (pos1 != pos2)
-    {
-        auto temp1 = pos1;
-        auto temp2 = pos2;
-        pos1 = next_pos(grid, prev1, pos1);
-        pos2 = next_pos(grid, prev2, pos2);
-        prev1 = temp1;
-        prev2 = temp2;
-        ++step_count;
-        // std::cout << "Step: " << step_count << '\n';
-        // std::cout << "1: " << prev1.first << ", " << prev1.second << " -> " << pos1.first << ", " << pos1.second << '\n';
-        // std::cout << "2: " << prev2.first << ", " << prev2.second << " -> " << pos2.first << ", " << pos2.second << '\n';
-        // std::cout << "---\n";
-    }
-
-    std::cout << "Part 1: " << step_count << std::endl; // 6968
-
+    part1(argv[1]);
+    part2(argv[1]);
     return 0;
 }
